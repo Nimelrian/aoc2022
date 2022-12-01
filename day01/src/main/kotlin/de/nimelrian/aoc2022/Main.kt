cadab37110272f9a12a4df4e38ed5757266d3590
Sebastian Kappen <sebastian@kappen.dev>
2022-12-01 18:58:06 +0100

    Formatting

--- day01/src/main/kotlin/de/nimelrian/aoc2022/Main.kt
+++ day01/src/main/kotlin/de/nimelrian/aoc2022/Main.kt
@@ -3,20 +3,20 @@ package de.nimelrian.aoc2022
 fun main() = aoc {
     part("/input") { lines ->
         lines.chunkByPredicate(String::isEmpty)
-            .map { elfSnacks -> elfSnacks
-                .asSequence()
-                .filterNot { it.isEmpty() }
-                .sumOf { it.toInt() }
+            .map { elfSnacks ->
+                elfSnacks.asSequence()
+                    .filterNot { it.isEmpty() }
+                    .sumOf { it.toInt() }
             }
             .max()
     }
 
     part("/input") { lines ->
         lines.chunkByPredicate(String::isEmpty)
-            .map { elfSnacks -> elfSnacks
-                .asSequence()
-                .filterNot { it.isEmpty() }
-                .sumOf { it.toInt() }
+            .map { elfSnacks ->
+                elfSnacks.asSequence()
+                    .filterNot { it.isEmpty() }
+                    .sumOf { it.toInt() }
             }
             .sortedDescending()
             .take(3)
