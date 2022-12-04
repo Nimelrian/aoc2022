36eb4b3153b766b0e5d05392245232cbb807d7de
Sebastian Kappen <sebastian@kappen.dev>
2022-12-04 12:29:18 +0100

    Day 4

---
+++ day04/src/main/kotlin/de/nimelrian/aoc2022/Main.kt
@@ -0,0 +1,38 @@
+package de.nimelrian.aoc2022
+
+fun main() = aoc {
+    part("/input") { lines ->
+        lines.map { line ->
+            line.split(',')
+                .map { it.toIntRange() }
+        }
+            .count { (first, second) -> doRangesOverlapCompletely(first, second) }
+    }
+
+    part("/input") { lines ->
+        lines.map { line ->
+            line.split(',')
+                .map { it.toIntRange() }
+        }
+            .count { (first, second) -> doRangesOverlap(first, second) }
+    }
+}
+
+fun String.toIntRange(): IntRange {
+    val (start, end) = this.split('-')
+    return IntRange(start.toInt(), end.toInt())
+}
+
+fun doRangesOverlapCompletely(first: IntRange, second: IntRange) = first.contains(second) || second.contains(first)
+fun doRangesOverlap(first: IntRange, second: IntRange) = first.overlaps(second)
+
+fun IntRange.contains(other: IntRange): Boolean {
+    return other.contains(this.first) && other.contains(this.last)
+}
+
+fun IntRange.overlaps(other: IntRange): Boolean {
+    return other.contains(this.last)
+            || other.contains(this.first)
+            || this.contains(other.first)
+            || this.contains(other.last)
+}
